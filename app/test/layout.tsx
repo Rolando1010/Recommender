84f4cc812a11e1ec52bfd77b16368d6f67fca47e
Rolando1010 <rmcdesarrollo@gmail.com>
2023-01-31 23:17:54 -0600

    test layout mapping typed

--- app/test/layout.tsx
+++ app/test/layout.tsx
@@ -4,7 +4,7 @@ export default async ({ children }: any) => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const json = await response.json();
     console.log("fetch de datos");
-    return <>{json.map(d =>
+    return <>{json.map((d: any) =>
         <Link href={`/test/${d.id}`} key={d.id}>{d.name}</Link>
     )}{children}</>;
 }
